16ed9d650a3563efba419e3d528a2fb566f3fac5
canyonfoot <canyon.foot@gmail.com>
2024-11-12 17:58:49 +0000

    fix tests

--- watttime/api_test.py
+++ watttime/api_test.py
@@ -1,3 +1,4 @@
+# /bin/python3 -m watttime.api_test /home/annie.zhu/watttime-python-client-aer-algo/watttime/api_test.py 
 import os
 import time
 from datetime import date, datetime, timedelta
@@ -8,16 +9,16 @@ from pytz import UTC, timezone
 import matplotlib.pyplot as plt
 
 from watttime import (
-    WattTimeMyAccess,
-    WattTimeHistorical,
-    WattTimeForecast,
+    # WattTimeMyAccess,
+    # WattTimeHistorical,
+    # WattTimeForecast,
     WattTimeOptimizer,
-    RecalculatingWattTimeOptimizer, 
 )
 
 region = "PJM_NJ"
 username = os.getenv("WATTTIME_USER")
 password = os.getenv("WATTTIME_PASSWORD")
+
 wt_opt = WattTimeOptimizer(username, password)
 
 now = datetime.now(UTC)
@@ -30,19 +31,18 @@ basic_usage_plan = wt_opt.get_optimal_usage_plan(
     region=region,
     usage_window_start=window_start_test,
     usage_window_end=window_end_test,
-    usage_time_required_minutes=240,
+    usage_time_required_minutes=160,
     usage_power_kw=usage_power_kw,
     optimization_method="baseline",
 )
 print(basic_usage_plan["emissions_co2e_lb"].sum())
-
 
 print("Using DP Plan w/ fixed power rate (kW)")
 dp_usage_plan = wt_opt.get_optimal_usage_plan(
     region=region,
     usage_window_start=window_start_test,
     usage_window_end=window_end_test,
-    usage_time_required_minutes=240,
+    usage_time_required_minutes=160,
     usage_power_kw=usage_power_kw,
     optimization_method="sophisticated",
 )
@@ -53,7 +53,7 @@ dp_usage_plan_2 = wt_opt.get_optimal_usage_plan(
     region=region,
     usage_window_start=window_start_test,
     usage_window_end=window_end_test,
-    usage_time_required_minutes=240,
+    usage_time_required_minutes=160,
     usage_power_kw=usage_power_kw,
     usage_time_uncertainty_minutes=180,
     optimization_method="sophisticated",
@@ -75,3 +75,69 @@ dp_usage_plan_3 = wt_opt.get_optimal_usage_plan(
     optimization_method="auto",
 )
 print(dp_usage_plan_3["emissions_co2e_lb"].sum())
+
+
+print("Using auto mode, but with a non-round usage time minutes")
+dp_usage_plan_4 = wt_opt.get_optimal_usage_plan(
+    region=region,
+    usage_window_start=window_start_test,
+    usage_window_end=window_end_test,
+    usage_time_required_minutes=7,
+    usage_power_kw=usage_power_kw,
+    optimization_method="auto",
+)
+print(dp_usage_plan_4.sum())
+
+
+print("Using auto mode, but constrained to a single contiguous interval")
+dp_usage_plan_5 = wt_opt.get_optimal_usage_plan(
+    region=region,
+    usage_window_start=window_start_test,
+    usage_window_end=window_end_test,
+    usage_time_required_minutes=160,
+    usage_power_kw=usage_power_kw,
+    charge_per_interval=[(160,160)],
+    optimization_method="auto",
+)
+print(dp_usage_plan_5["usage"].tolist())
+print(dp_usage_plan_5.sum())
+
+print("Using auto mode, but constrained to two contiguous intervals with fixed length constraints")
+dp_usage_plan_6 = wt_opt.get_optimal_usage_plan(
+    region=region,
+    usage_window_start=window_start_test,
+    usage_window_end=window_end_test,
+    usage_time_required_minutes=160,
+    usage_power_kw=usage_power_kw,
+    charge_per_interval=[60,100],
+    optimization_method="auto",
+)
+print(dp_usage_plan_6["usage"].tolist())
+print(dp_usage_plan_6.sum())
+
+print("Using auto mode, but constrained to two contiguous intervals with variable length constraints")
+dp_usage_plan_7 = wt_opt.get_optimal_usage_plan(
+    region=region,
+    usage_window_start=window_start_test,
+    usage_window_end=window_end_test,
+    usage_time_required_minutes=160,
+    usage_power_kw=usage_power_kw,
+    charge_per_interval=[(60,100),(60,100)],
+    optimization_method="auto",
+)
+print(dp_usage_plan_7["usage"].tolist())
+print(dp_usage_plan_7.sum())
+
+print("Using auto mode, but constrained to two contiguous intervals with variable length constraints, and doesn't need to use all intervals")
+dp_usage_plan_8 = wt_opt.get_optimal_usage_plan(
+    region=region,
+    usage_window_start=window_start_test,
+    usage_window_end=window_end_test,
+    usage_time_required_minutes=160,
+    usage_power_kw=usage_power_kw,
+    charge_per_interval=[(40,None)]*4,
+    use_all_intervals=False,
+    optimization_method="auto",
+)
+print(dp_usage_plan_8["usage"].tolist())
+print(dp_usage_plan_8.sum())
